f56cd3f12103bb27d9ffd920ad4f0e5dfb36e3b8
Mellorsssss <1026579097@qq.com>
2023-02-28 02:24:32 -0500

    feat(Db721Reader): basic parse
    Db721Reader is able to fetch block and reconstruct the row.

--- cmudb/extensions/db721_fdw/src/dog.cpp
+++ cmudb/extensions/db721_fdw/src/dog.cpp
@@ -72,6 +72,7 @@ namespace Db721
 
     // maintain the basic information
     meta_info_.maxvalue_block_ = meta_["Max Values Per Block"];
+    meta_info_.table_name_ = std::move(meta_["Table"]);
 
     // metadata["Columns"]["Column Name"] -> column data(json)
     auto &columns = meta_["Columns"];
@@ -83,21 +84,25 @@ namespace Db721
       column_index_[column.key()] = meta_info_.column_num_;
       meta_info_.column_num_++;
 
-      if (type_map.find(column.value()["type"]) == type_map.end())
+      if (type_map.find(column.value()["type"]) == type_map.end() || type_size.find(column.value()["type"]) == type_size.end())
       {
         elog(ERROR, "undefined type %s", column.value()["type"]);
       }
 
       column_info col_info;
       col_info.column_type = type_map[column.value()["type"]];
+      col_info.type_size = type_size[column.value()["type"]];
       col_info.column_name = column.key();
       col_info.start_offset = column.value()["start_offset"];
       col_info.block_size.resize(column.value()["num_blocks"]);
-      for (auto& block: column.value()["block_stats"].items()){
+
+      for (auto &block : column.value()["block_stats"].items())
+      {
         int idx = atoi(block.key().c_str());
-        
-        if (idx >= static_cast<int>(col_info.block_size.size())){
-          elog(ERROR, "invalid block index %d", idx); 
+
+        if (idx >= static_cast<int>(col_info.block_size.size()))
+        {
+          elog(ERROR, "invalid block index %d", idx);
         }
         col_info.block_size[idx] = block.value()["num"];
       }
@@ -110,20 +115,125 @@ namespace Db721
         meta_info_.num_block_ = column.value()["num_blocks"];
       }
     }
+
+    meta_info_.dump();
+    Reset();
+    elog(LOG, "Db721 of init success");
   }
 
-  ReadStatus Db721Reader::ReadColumn(int col_idx)
+  inline ReadStatus Db721Reader::ReadColumn(int col_idx)
   {
+    if (cur_block_idx_ >= meta_info_.num_block_)
+    {
+      elog(ERROR, "fail to read block %d", cur_block_idx_);
+    }
+
+    int offset;
+    Status s = GetColumnOffset(col_idx, cur_block_idx_, offset);
+    if (s != Status::SUCCESS)
+    {
+      elog(ERROR, "fail to get the column(%d, %d) offset", col_idx, cur_block_idx_);
+    }
+
+    if (column_data_[col_idx] == nullptr)
+    {
+      // allocate the size of maximun possible chunk size
+      column_data_[col_idx] = palloc0(meta_info_.maxvalue_block_ * column_type_[col_idx].type_size);
+    }
+
+    int read_size = column_type_[col_idx].block_size[cur_block_idx_] * column_type_[col_idx].type_size;
+    reader_->Read(offset, read_size, column_data_[col_idx]);
+
+    elog(LOG, "read column %d at %d, total %d bytes", col_idx, offset, read_size);
+    return ReadStatus::RS_OK;
   }
 
-  ReadStatus Db721Reader::ReadNextBlockBatch()
+  inline ReadStatus Db721Reader::ReadNextBlockBatch()
   {
+    if (cur_block_idx_ == meta_info_.num_block_)
+    {
+      return ReadStatus::RS_EOF;
+    }
+
+    for (int col_idx = 0; col_idx < meta_info_.column_num_; col_idx++)
+    {
+      if (ReadStatus::RS_OK != ReadColumn(col_idx))
+      {
+        elog(LOG, "fail to read column %d", col_idx);
+        return ReadStatus::RS_ERR;
+      }
+    }
+
+    total_row_ = column_type_[0].block_size[cur_block_idx_]; // hack: assume all the block with same idx hold the same number of values
+    row_ = 0;
+    cur_block_idx_++;
+
+    return ReadStatus::RS_OK;
   }
 
   /* ReadNextRow */
   ReadStatus Db721Reader::ReadNextRow(TupleTableSlot *slot)
   {
     // TODO: make up a row from the in-memory block data
+    if (row_ >= total_row_)
+    {
+      ReadStatus s;
+      if (ReadStatus::RS_OK != (s = ReadNextBlockBatch()))
+      {
+        if (s != ReadStatus::RS_EOF)
+        {
+          elog(LOG, "fail to read the next block batch");
+        }
+        else
+        {
+          elog(LOG, "no more rows");
+        }
+        return s;
+      }
+    }
+
+    // TODO: put all the rows into the slot(order?)
+
+    // simulate the row
+    std::cout << row_ << "| ";
+    int vi;
+    float vf;
+    char *vc;
+    for (int col_idx = 0; col_idx < meta_info_.column_num_; ++col_idx)
+    {
+      switch (column_type_[col_idx].column_type)
+      {
+      case DataType::INT:
+        vi = ((int *)(column_data_[col_idx]))[row_];
+        std::cout << vi << " | ";
+        break;
+      case DataType::FLOAT:
+        vf = ((float *)(column_data_[col_idx]))[row_];
+        std::cout << vf << " | ";
+        break;
+      case DataType::VARCHAR:
+        vc = ((char *)(column_data_[col_idx])) + 32 * row_;
+        std::cout << vc << " | ";
+        break;
+      default:
+        elog(ERROR, "unexpected datatype");
+      }
+    }
+    std::cout << "\n";
+    ++row_;
+
     return ReadStatus::RS_OK;
+  }
+
+  Db721Reader::Status Db721Reader::GetColumnOffset(int col_idx, int block_idx, int &offset)
+  {
+    if (col_idx >= meta_info_.column_num_)
+    {
+      return Status::FAILURE;
+    }
+
+    // hack: we assume all the block has the block size as the maxvalue_block except last one
+    offset = block_idx * meta_info_.maxvalue_block_ * column_type_[col_idx].type_size + column_type_[col_idx].start_offset;
+    return Status::SUCCESS;
   }
 }
--- cmudb/extensions/db721_fdw/src/dog.h
+++ cmudb/extensions/db721_fdw/src/dog.h
@@ -30,7 +30,7 @@ extern "C" {
 #include <unistd.h>
 #include <vector>
 
-using json = nlohmann::json;
+using json = nlohmann::ordered_json;
 
 class Dog
 {
@@ -83,6 +83,19 @@ namespace Db721
     RS_OK,
   };
 
+  struct Db721FdwPlanState
+  {
+    List *filenames;
+    List *attrs_sorted;
+    Bitmapset *attrs_used; /* attributes actually used in query */
+    bool use_mmap;
+    bool use_threads;
+    int32 max_open_files;
+    bool files_in_order;
+    // List *rowgroups; /* List of Lists (per filename) */
+    // uint64 matched_rows;
+  };
+
   class Db721Reader
   {
   private:
@@ -91,7 +104,13 @@ namespace Db721
       int column_num_;
       int maxvalue_block_;
       int num_block_; // for this project, all the column have the same number of blocks
+      std::string table_name_;
       // TODO: num_block_ can be dangerous in the scenario different block have different block value numbers
+
+      void dump()
+      {
+        elog(LOG, "table %s's meta data: %d columns, maxvalue per block is %d, has %d blocks in total", table_name_.c_str(), column_num_, maxvalue_block_, num_block_);
+      }
     } meta_info;
 
     enum DataType
@@ -102,6 +121,7 @@ namespace Db721
     };
 
     static inline std::map<std::string, DataType> type_map = {{"float", DataType::FLOAT}, {"int", DataType::INT}, {"str", DataType::VARCHAR}};
+    static inline std::map<std::string, int> type_size = {{"float", 4}, {"int", 4}, {"str", 32}};
 
     enum Status
     {
@@ -113,12 +133,18 @@ namespace Db721
     {
       DataType column_type;
       int start_offset;
+      int type_size;
       std::string column_name;
       std::vector<int> block_size; // invariant: block_size.size() == meta_info_.num_block_
 
       void dump()
       {
-        elog(LOG, "column: %s, start offset: %d, type: %d, block num: %d", column_name.c_str(), start_offset, static_cast<int>(column_type), static_cast<int>(block_size.size()));
+        elog(LOG, "column: %s, start offset: %d, type: %d(%d bytes), block num: %d", column_name.c_str(), start_offset, static_cast<int>(column_type), type_size, static_cast<int>(block_size.size()));
+
+        for (size_t ind = 0; ind < block_size.size(); ++ind)
+        {
+          elog(LOG, "column: %s block %ld-th's size is %d", column_name.c_str(), ind, block_size[ind]);
+        }
       }
     } column_info;
 
@@ -133,7 +159,24 @@ namespace Db721
   public:
     void Init();
 
+    void Reset()
+    {
+      total_row_ = 0;
+      row_ = 0;
+      cur_block_idx_ = 0;
+      column_data_.resize(meta_info_.column_num_, nullptr);
+    }
+
     void Debug() const;
+
+    /* ReadNextRow */
+    ReadStatus ReadNextRow(TupleTableSlot *slot);
+
+    int RowNumber() const
+    {
+      // TODO: this is just an approximation
+      return meta_info_.num_block_ * meta_info_.maxvalue_block_;
+    }
 
   private:
     /* ReadColumn fetch all column data */
@@ -142,17 +185,17 @@ namespace Db721
     /* ReadNextBlock fetch all the blocks into memory according to the schema*/
     ReadStatus ReadNextBlockBatch();
 
-    /* ReadNextRow */
-    ReadStatus ReadNextRow(TupleTableSlot *slot);
-
-    Status GetColumnOffset(int col_idx, int block_idx);
+    Status GetColumnOffset(int col_idx, int block_idx, int &offset);
 
   private:
     std::unique_ptr<FileReader> reader_;
-    std::vector<void *> column_data_; // current batch of column data
+
+    /* current batch of column data, duckdb treat every data as DataChunk
+       for simplicity, just use void* here */
+    std::vector<void *> column_data_;
 
     /* meta data */
-    json meta_;
+    json meta_; // cache this, maybe we should drop it?
     meta_info meta_info_{.column_num_ = 0, .maxvalue_block_ = 0, .num_block_ = 0};
 
     /* map column name to column index */
@@ -163,5 +206,6 @@ namespace Db721
     /* read state */
     int total_row_{0}; // total row number of current in-memory(column_data_) block
     int row_{0};       // invariant: row_  <= total_row_
+    int cur_block_idx_{0};
   };
 }
